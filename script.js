485773944c88752335e277bc5cc9acc27ddf873d
ghoshrohit72 <rohitofficial95@outlook.com>
2024-05-20 22:51:53 +0530

    Final Commit

--- script.js
+++ script.js
@@ -1,11 +1,38 @@
-const video =  document.getElementById("video");
+const video = document.getElementById("video");
+
+Promise.all([
+  faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
+  faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
+  faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
+  faceapi.nets.faceExpressionNet.loadFromUri("/models"),
+]).then(startVideo);
 
 function startVideo() {
-    navigator.getUserMedia(
-        { video: {}},
-        stream => video.srcObject = stream,
-        err => console.error("error")
-    )
+  navigator.getUserMedia(
+    { video: {} },
+    (stream) => (video.srcObject = stream),
+    (err) => console.error(err)
+  );
 }
 
-startVideo()
+video.addEventListener("play", () => {
+  const canvas = faceapi.createCanvasFromMedia(video);
+  document.body.append(canvas);
+  const displaySize = { width: video.width, height: video.height };
+
+  setInterval(async () => {
+    const detections = await faceapi
+      .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+      .withFaceLandmarks()
+      .withFaceExpressions();
+
+    faceapi.matchDimensions(canvas, displaySize)
+    // console.log(detections);
+
+    const resizeDectections = faceapi.resizeResults(detections, displaySize);
+    canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+    faceapi.draw.drawDetections(canvas, resizeDectections);
+    faceapi.draw.drawFaceLandmarks(canvas, resizeDectections);
+    faceapi.draw.drawFaceExpressions(canvas, resizeDectections);
+  }, 100);
+});
